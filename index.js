227b153e053c412fd3b497556990d591f9cabd8c
izzie <izzienjeri12@gmail.com>
2023-09-30 01:05:37 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const name =['Joy', 'Rose', 'Kipruto', 'Chelsea']
+function writeCards(name, eventName) {
+    let messagesArray = [];
+    for (let i = 0; i < name.length; i++) {
+      messagesArray.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messagesArray;
+  }
+
+  function countDown(startingPositiveInteger) {
+    while (startingPositiveInteger >= 0) {
+      console.log(startingPositiveInteger);
+      startingPositiveInteger--;
+    }
+  }
